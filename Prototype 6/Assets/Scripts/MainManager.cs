d0a4537de71e1713371e4029f09323b7d2d76a6f
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2022-01-13 21:24:51 +0900

    Prototype 6 Fin

--- Prototype 6/Assets/Scripts/MainManager.cs
+++ Prototype 6/Assets/Scripts/MainManager.cs
@@ -1,13 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
     public static MainManager Instance;
     public Color TeamColor;
 
-    
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))  // IF File(path) is exist.
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
+    }
+
 
     private void Awake() 
     {
@@ -20,5 +52,7 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        LoadColor();
     }
 }
